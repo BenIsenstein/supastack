6c7c0d58d58000fab066346bc68760dd417101d7
Ben Isenstein <ben.isenstein@gmail.com>
2023-12-07 03:12:06 -0700

    feat: mvp

--- main.py
+++ main.py
@@ -1,14 +1,123 @@
 import subprocess
+import sys
+import os
+import getpass
+import re
+import time
 
 app_name = input('Project name: ')
+public_or_private = input('Would you like your new Github repo to be public or private? Type either "public" or "private". ')
 
-command = "git clone https://github.com/BenIsenstein/open-data-react-supabase.git"
-
-commands = command.split()
-commands.append(f"'{app_name}'")
-
-subprocess.Popen(
-    commands,
-    stderr=subprocess.STDOUT,
-    stdout=subprocess.PIPE,
+process = subprocess.Popen(
+    f'git clone https://github.com/BenIsenstein/open-data-react-supabase.git "{app_name}"',
+    text=True,
+    shell=True
 )
+
+process.wait()
+
+if process.returncode != 0:
+    sys.exit(process.returncode)
+
+os.chdir(app_name)
+
+process = subprocess.Popen(
+    f"rm -rf .git && git init && git add . && git commit -m 'feat: create repo'",
+    text=True,
+    shell=True
+)
+
+process.wait()
+
+if process.returncode != 0:
+    sys.exit(process.returncode)
+
+process = subprocess.Popen(
+    f"gh repo create '{app_name}' --{public_or_private} --source . --description 'Web app created with Supastack' --push",
+    text=True,
+    shell=True
+)
+
+process.wait()
+
+if process.returncode != 0:
+    sys.exit(process.returncode)
+
+process = subprocess.Popen(
+    'git remote get-url origin',
+    shell=True,
+    text=True,
+    stdout=subprocess.PIPE
+)
+
+repo_url, stderr = process.communicate()
+
+if process.returncode != 0:
+    sys.exit(process.returncode)
+
+supabase_org = input('Please paste your supabase organization ID. ')
+supabase_pswd = getpass.getpass('Please paste a db password for supabase. ')
+supabase_plan = input('Which supabase plan are you on? Type "free" or "pro". (free) ') or 'free'
+
+process = subprocess.Popen(
+    f'supabase projects create "{app_name}"  --db-password {supabase_pswd} --org-id {supabase_org} --plan {supabase_plan} --region ca-central-1',
+    shell=True,
+    text=True,
+    stdout=subprocess.PIPE
+)
+
+supabase_project_output, stderr = process.communicate()
+
+if process.returncode != 0:
+    sys.exit(process.returncode)
+
+supabase_project_url = supabase_project_output.split()[-1]
+supabase_project_id = supabase_project_output.split('/')[-1]
+
+try_count = 0
+
+while True:
+    time.sleep(try_count * 2)
+
+    process = subprocess.Popen(
+        f'supabase projects api-keys --project-ref {supabase_project_id}',
+        shell=True,
+        text=True,
+        stdout=subprocess.PIPE
+    )
+
+    supabase_keys_output, stderr = process.communicate()
+
+    if process.returncode != 0:
+        if try_count > 10:
+            sys.exit(process.returncode)
+        else:
+            try_count += 1
+            continue
+    break
+        
+key = re.search(r'([a-zA-Z0-9_-]+)\.([a-zA-Z0-9_-]+)\.([a-zA-Z0-9_-]+)', supabase_keys_output)
+start, end = key.span()
+supabase_anon_key = supabase_keys_output[start:end]
+
+print(f"Created supabase project at {supabase_project_url}")
+
+os.chdir('terraform')
+
+with open('config.tfvars', 'w+') as file:
+    file.write(f'project_name = "{app_name}"\n')
+    file.write(f'repo_url = "{repo_url.strip()}"\n')
+    file.write(f'supabase_key = "{supabase_anon_key}"\n')
+    file.write(f'supabase_url = "https://{supabase_project_id}.supabase.co"')
+
+process = subprocess.Popen(
+    f"terraform init && terraform apply --var-file config.tfvars",
+    text=True,
+    shell=True
+)
+
+process.wait()
+
+if process.returncode != 0:
+    sys.exit(process.returncode)
+
